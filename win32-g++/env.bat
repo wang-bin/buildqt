873ff36d7635b10633f283367c8aaa7f2876a35e
wang-bin <wbsecg1@gmail.com>
2012-12-23 01:35:08 +0800

    var

--- win32-g++/env.bat
+++ win32-g++/env.bat
@@ -3,17 +3,19 @@
 @echo NOTE: ensure no sh.exe in your %%PATH%%
 @echo ...
 @echo off
+set MINGWPATH=G:\MinGW\bin
+set QTSRCDIR=G:\dev\qtbase
 set INCLUDE=g:\build\openssl\include
 set LIB=g:\build\openssl\lib
 set QMAKESPEC=
 set QTDIR=
 set MAKE_COMMAND=
 
-set PATH=%CD%\bin;G:\GNUWin32\bin;G:\QtSDK\MinGW\bin;G:\strawberry\perl\bin;C:\Python27;C:\Program Files (x86)\Git\cmd;%SystemRoot%\system32;%SystemRoot%;G:\dev\qtbase
+set PATH=%CD%\bin;G:\GNUWin32\bin;%MINGWPATH%;G:\strawberry\perl\bin;C:\Python27;C:\Program Files (x86)\Git\cmd;%SystemRoot%\system32;%SystemRoot%;%QTSRCDIR%
 
 set QT5OPT=-release -opensource -confirm-license -platform win32-g++ -developer-build -ltcg -c++11 -no-freetype -opengl desktop -openssl -qt-sql-sqlite -qt-zlib -qt-style-windowsxp -qt-style-windowsvista -nomake tests -nomake demos -nomake examples
 set QT5OPT_NOCXX11=-release -opensource -confirm-license -platform win32-g++ -developer-build -ltcg -no-c++11 -no-freetype -opengl desktop -openssl -qt-sql-sqlite -qt-zlib -qt-style-windowsxp -qt-style-windowsvista -nomake tests -nomake demos -nomake examples
-set QT4CONFIG=-developer-build  -opensource -confirm-license -platform win32-g++ -ltcg -release -shared -fast -stl -qt-sql-sqlite -no-qt3support -no-xmlpatterns -no-declarative-debug -nomake demos -nomake examples -nomake docs  -nomake tests -qt-libpng -qt-libtiff -qt-libjpeg -qt-libmng -qt-zlib -script -scripttools -no-webkit -largefile  -qt-style-windowsxp -qt-style-windowsvista  -opengl desktop -openssl -graphicssystem opengl
+set QT4OPT=-developer-build  -opensource -confirm-license -platform win32-g++ -ltcg -release -shared -fast -stl -qt-sql-sqlite -no-qt3support -no-xmlpatterns -no-declarative-debug -nomake demos -nomake examples -nomake docs  -nomake tests -qt-libpng -qt-libtiff -qt-libjpeg -qt-libmng -qt-zlib -script -scripttools -no-webkit -largefile  -qt-style-windowsxp -qt-style-windowsvista  -opengl desktop -openssl -graphicssystem opengl
 @echo on
 
 @echo INCLUDE=g:\build\openssl\include
