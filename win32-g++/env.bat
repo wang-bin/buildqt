df8a745a757b282417f0618c4651850fcbda27cc
wang-bin <wbsecg1@gmail.com>
2013-05-26 20:08:42 +0800

    fix vc settings. add vc shortcut

--- win32-g++/env.bat
+++ win32-g++/env.bat
@@ -4,24 +4,40 @@
 set this=%~nx0
 set compiler=%1
 if "%compiler%" == "" (goto help)
-if "%compiler%" == "g++" (set QMAKESPEC=win32-g++) else set QMAKESPEC=win32-ms%compiler%
+if "%compiler%" == "g++" set QMAKESPECNAME=win32-g++
+if "%compiler%" == "clang" set QMAKESPECNAME=win32-clang
+if "%compiler%" == "vc" set QMAKESPECNAME=win32-ms%compiler%%2
+
 @echo compiler: %compiler%
 
 
 set GNUWIN32_BIN=G:\GNUWin32\bin
 set PERL_BIN=G:\strawberry\perl\bin
-set MINGW_BIN=C:\MinGW\bin
-set QTSRCDIR=G:\dev\qtbase
-set INCLUDE_SSL=G:\software\vs2012-x86\build\include
-set LIB_SSL=G:\software\vs2012-x86\build\lib
+set MINGW_BIN=G:\MinGW\mingw64\bin
+set QTSRCDIR=G:\dev\qt
+set INCLUDE_SSL=
+set LIB_SSL=
 
 set QTDIR=
 
-if "%QMAKESPEC%" == "win32-g++" (goto setgcc) else goto setvc
+:: for clang
+set CPATH=%CD%\include\c++\4.8.0;%CD%\x86_64-w64-mingw32\include;%CD%\include\c++\4.8.0\x86_64-w64-mingw32
+set LIBRARY_PATH=%CD%\x86_64-w64-mingw32\lib
+
+if "%QMAKESPECNAME%" == "win32-g++" (
+	echo "%QMAKESPECNAME%"
+	goto setgcc
+) else if "%QMAKESPECNAME%" == "win32-clang" (
+	echo "%QMAKESPECNAME%"
+	goto setgcc
+) else (
+	echo "%QMAKESPECNAME%"
+	goto setvc
+)
 
 :help
 @echo Usage:
-@echo call %this% "g++|vc2012|vc2010|..."
+@echo call %this% "g++|clang|vc 2012|vc 2010|..."
 @echo You may have to edit some vars in %this%
 goto END
 
@@ -38,13 +54,15 @@ goto setqt
 @echo NOTE: ensure no sh.exe in your %%PATH%%
 @echo ...
 @set MAKE_COMMAND=mingw32-make -j4
-set PATH=%CD%\bin;%GNUWIN32_BIN%;%MINGW_BIN%;%PERL_BIN%;%SystemRoot%\system32;%SystemRoot%;%QTSRCDIR%
+set PATH=%CD%\bin;%GNUWIN32_BIN%;%MINGW_BIN%;%PERL_BIN%;%SystemRoot%\system32;%SystemRoot%;%QTSRCDIR%;
 goto setqt
 
 :setqt
-set QT5OPT=-release -opensource -confirm-license -platform %QMAKESPEC%  -developer-build -ltcg -c++11 -no-freetype -opengl desktop -openssl -qt-sql-sqlite -qt-zlib -qt-style-windowsxp -qt-style-windowsvista -nomake tests -nomake demos -nomake examples
-set QT5OPT_NOCXX11=-release -opensource -confirm-license -platform %QMAKESPEC% -developer-build -ltcg -no-c++11 -no-freetype -opengl desktop -openssl -qt-sql-sqlite -qt-zlib -qt-style-windowsxp -qt-style-windowsvista -nomake tests -nomake demos -nomake examples
-set QT4OPT=-developer-build  -opensource -confirm-license -platform %QMAKESPEC% -ltcg -release -shared -fast -stl -qt-sql-sqlite -no-qt3support -no-xmlpatterns -no-declarative-debug -nomake demos -nomake examples -nomake docs  -nomake tests -qt-libpng -qt-libtiff -qt-libjpeg -qt-libmng -qt-zlib -script -scripttools -no-webkit  -qt-style-windowsxp -qt-style-windowsvista  -opengl desktop -openssl -graphicssystem opengl
+set QT5OPT=-release -opensource -confirm-license -platform %QMAKESPECNAME%  -developer-build -ltcg -c++11 -no-freetype -opengl desktop -openssl -qt-sql-sqlite -qt-zlib -qt-style-windowsxp -qt-style-windowsvista -nomake tests -nomake demos -nomake examples
+set QT5OPT_NOCXX11=-release -opensource -confirm-license -platform %QMAKESPECNAME% -developer-build -ltcg -no-c++11 -no-freetype -opengl desktop -openssl -qt-sql-sqlite -qt-zlib -qt-style-windowsxp -qt-style-windowsvista -nomake tests -nomake demos -nomake examples
+set QT4OPT=-developer-build  -opensource -confirm-license -platform %QMAKESPECNAME% -ltcg -release -shared -fast -stl -qt-sql-sqlite -no-qt3support -no-xmlpatterns -no-declarative-debug -nomake demos -nomake examples -nomake docs  -nomake tests -qt-libpng -qt-libtiff -qt-libjpeg -qt-libmng -qt-zlib -script -scripttools -no-webkit  -qt-style-windowsxp -qt-style-windowsvista  -opengl desktop -openssl
+
+:: -graphicssystem opengl
 
 set INCLUDE=%INCLUDE%;%INCLUDE_SSL%
 set LIB=%LIB%;%LIB_SSL%
