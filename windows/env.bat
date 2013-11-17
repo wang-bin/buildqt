bfc4b7f72b64b25e8759231200e8007897a4586e
wang-bin <wbsecg1@gmail.com>
2013-11-17 13:02:04 +0800

    correct some paths
    TODO: detech arch

--- windows/env.bat
+++ windows/env.bat
@@ -1,8 +1,10 @@
 @echo wbsecg1@gmail.com 2012-12-18
 :: http://stackoverflow.com/questions/3432851/dos-bat-file-equivalent-to-unix-basename-command
 @echo off
-set this=%~nx0
+set THIS=%~nx0
+set THIS_DIR=%~dp0
 set compiler=%1
+
 if "%compiler%" == "" (goto help)
 if "%compiler%" == "g++" set QMAKESPECNAME=win32-g++
 if "%compiler%" == "clang" set QMAKESPECNAME=win32-clang
@@ -10,19 +12,22 @@ if "%compiler%" == "vc" set QMAKESPECNAME=win32-ms%compiler%%2
 
 @echo compiler: %compiler%
 
-
+:: TODO: XP compat for vs>2012
+set DEPEND_DIR=%THIS_DIR%depends
 set GNUWIN32_BIN=G:\GNUWin32\bin
 set PERL_BIN=G:\strawberry\perl\bin
-set MINGW_BIN=G:\MinGW\mingw64\bin
-set QTSRCDIR=G:\dev\qt
-set INCLUDE_SSL=
-set LIB_SSL=
-
+set MINGW_BIN=G:\MinGW\MinGW\bin
+set QTSRCDIR=G:\dev\qtbase
+set OPENSSL_DIR=%DEPEND_DIR%\OpenSSL
+:: INCLUDE, LIB, PATH may be auto define if DXSDK_DIR is set
+set DXSDK_DIR=%DEPEND_DIR%\DXSDK\
 set QTDIR=
+set WINSDKDIR=C:\Program Files (x86)\Microsoft SDKs\Windows\v7.1A
 
+set XP_OPTS=
 :: for clang
-set CPATH=%CD%\include\c++\4.8.0;%CD%\x86_64-w64-mingw32\include;%CD%\include\c++\4.8.0\x86_64-w64-mingw32
-set LIBRARY_PATH=%CD%\x86_64-w64-mingw32\lib
+::set CPATH=%THIS_DIR%include\c++\4.8.0;%THIS_DIR%x86_64-w64-mingw32\include;%THIS_DIR%include\c++\4.8.0\x86_64-w64-mingw32
+::set LIBRARY_PATH=%THIS_DIR%x86_64-w64-mingw32\lib
 
 if "%QMAKESPECNAME%" == "win32-g++" (
 	echo "%QMAKESPECNAME%"
@@ -32,12 +37,18 @@ if "%QMAKESPECNAME%" == "win32-g++" (
 	goto setgcc
 ) else (
 	echo "%QMAKESPECNAME%"
+	if "%3" == "-xp" (
+		set INCLUDE=%VCINSTALLDIR%INCLUDE;"%WINSDKDIR%\Include"
+		set LIB=%VCINSTALLDIR%LIB;"%WINSDKDIR%\lib"
+		set XP_OPTS=-xp
+	)
+		
 	goto setvc
 )
 
 :help
 @echo Usage:
-@echo call %this% "g++|clang|vc 2012|vc 2010|..."
+@echo call %this% "g++|clang|vc 2012|vc 2010|vc 2012|vc 2013..."
 @echo You may have to edit some vars in %this%
 goto END
 
@@ -46,7 +57,7 @@ goto END
 @echo ...
 :: may be called from vc prompt, so %PATH% should be kept
 :: GNUWin32's link.exe conflicts with vc, we should not add it to PATH
-set PATH=%CD%\bin;%PERL_BIN%;%SystemRoot%\system32;%SystemRoot%;%QTSRCDIR%;%PATH%
+set PATH=%THIS_DIR%bin;%PERL_BIN%;%SystemRoot%\system32;%SystemRoot%;%QTSRCDIR%;%PATH%
 goto setqt
 
 :setgcc
@@ -54,18 +65,23 @@ goto setqt
 @echo NOTE: ensure no sh.exe in your %%PATH%%
 @echo ...
 @set MAKE_COMMAND=mingw32-make -j4
-set PATH=%CD%\bin;%GNUWIN32_BIN%;%MINGW_BIN%;%PERL_BIN%;%SystemRoot%\system32;%SystemRoot%;%QTSRCDIR%;
+set PATH=%THIS_DIR%bin;%GNUWIN32_BIN%;%MINGW_BIN%;%PERL_BIN%;%SystemRoot%\system32;%SystemRoot%;%QTSRCDIR%;
 goto setqt
 
 :setqt
-set QT5OPT=-release -opensource -confirm-license -platform %QMAKESPECNAME%  -developer-build -ltcg -c++11 -no-freetype -opengl desktop -openssl -qt-sql-sqlite -qt-zlib -qt-style-windowsxp -qt-style-windowsvista -nomake tests -nomake demos -nomake examples
-set QT5OPT_NOCXX11=-release -opensource -confirm-license -platform %QMAKESPECNAME% -developer-build -ltcg -no-c++11 -no-freetype -opengl desktop -openssl -qt-sql-sqlite -qt-zlib -qt-style-windowsxp -qt-style-windowsvista -nomake tests -nomake demos -nomake examples
-set QT4OPT=-developer-build  -opensource -confirm-license -platform %QMAKESPECNAME% -ltcg -release -shared -fast -stl -qt-sql-sqlite -no-qt3support -no-xmlpatterns -no-declarative-debug -nomake demos -nomake examples -nomake docs  -nomake tests -qt-libpng -qt-libtiff -qt-libjpeg -qt-libmng -qt-zlib -script -scripttools -no-webkit  -qt-style-windowsxp -qt-style-windowsvista  -opengl desktop -openssl
+md qt-%QMAKESPECNAME%
+cd qt-%QMAKESPECNAME%
+set PATH=%THIS_DIR%qt-%QMAKESPECNAME%\bin;%PATH%
+
+set QT5OPT=-release -opensource -confirm-license -platform %QMAKESPECNAME%  -developer-build -ltcg -c++11 -no-freetype -opengl es2 -angle -openssl -qt-sql-sqlite  -no-iconv -qt-style-windowsvista -nomake tests  -nomake examples  -mp -openssl %XP_OPTS%
+set QT5OPT_NOCXX11=-release -opensource -confirm-license -platform %QMAKESPECNAME% -developer-build -ltcg -no-c++11 -no-freetype -opengl es2 -angle -openssl -qt-sql-sqlite  -qt-style-windowsxp -qt-style-windowsvista -nomake tests - -nomake examples %XP_OPTS%
+set QT4OPT=-developer-build  -opensource -confirm-license -platform %QMAKESPECNAME% -ltcg -release -shared -fast -stl -qt-sql-sqlite -no-qt3support -no-xmlpatterns -no-declarative-debug -nomake demos -nomake examples -nomake docs  -nomake tests -qt-libpng -qt-libtiff -qt-libjpeg -qt-libmng -script -scripttools -no-webkit  -qt-style-windowsxp -qt-style-windowsvista  -opengl desktop -openssl %XP_OPTS%
 
 :: -graphicssystem opengl
 
-set INCLUDE=%INCLUDE%;%INCLUDE_SSL%
-set LIB=%LIB%;%LIB_SSL%
+set INCLUDE=%INCLUDE%;%OPENSSL_DIR%\include;%DXSDK_DIR%Include
+set LIB=%LIB%;%OPENSSL_DIR%\lib;%DXSDK_DIR%Lib\x86
+set PATH=%PATH%;%DXSDK_DIR%Utilities\bin\x86
 @echo PATH=%PATH%
 @echo INCLUDE=%INCLUDE%
 @echo LIB=%LIB%
