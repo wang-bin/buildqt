b964f1275235fb1bcf5bbfee77f56a89cda452dc
wang-bin <wbsecg1@gmail.com>
2014-02-17 19:41:36 +0800

    build dir depends on toolchains. default desktop gl. add py
    Qt5.2.x angle build failed with mingw. so switch to desktop.
    TODO: different dir for opengl and gles2

--- windows/env.bat
+++ windows/env.bat
@@ -15,24 +15,32 @@ if "%compiler%" == "vc" set QMAKESPECNAME=win32-ms%compiler%%2
 
 @echo compiler: %compiler%
 
+:: build from release package using vc with xp support need add qtbase/.gitignore to rebuild configure.exe
 :: TODO: XP compat for vs>2012
+set QTSRCDIR=D:\qt-everywhere-opensource-src-5.2.1
 set DEPEND_DIR=%THIS_DIR%depends
-set GNUWIN32_BIN=G:\GNUWin32\bin
+set PYTHON_BIN=C:\Python27
+set GNUWIN32_BIN=%QTSRCDIR%\gnuwin32\bin
 set PERL_BIN=G:\strawberry\perl\bin
 set MINGW_BIN=G:\MinGW\MinGW\bin
-set QTSRCDIR=G:\dev\qtbase
 set OPENSSL_DIR=%DEPEND_DIR%\OpenSSL
 :: INCLUDE, LIB, PATH may be auto define if DXSDK_DIR is set
 set QTDIR=
 
+set VCVER=%2
 if "%compiler%" == "vc" (
-	if %2 lss 2012 (
+	set BUILD_DIR_SUFFIX=%QMAKESPECNAME%
+:: compare %2 with 2012 error if %2 not set
+	if %VCVER lss 2012 (
         set DXSDK_DIR=%DEPEND_DIR%\DXSDK\
 	) else (
 	    set DXSDK_DIR=
 	)
 )
-if "%compiler%" == "g++" set DXSDK_DIR=%DEPEND_DIR%\DXSDK\
+if "%compiler%" == "g++" (
+    set DXSDK_DIR=%DEPEND_DIR%\DXSDK\
+    set BUILD_DIR_SUFFIX=win32
+)
 if "%compiler%" == "clang" set DXSDK_DIR=%DEPEND_DIR%\DXSDK\
 
 :: WINSDK_DIR can not contain '(' and ')'. e.g. C:\Program Files (x86)\Microsoft SDKs\Windows\v7.1A is wrong. you may move it to another place
@@ -42,7 +50,8 @@ set WINSDK_DIR=C:\dev\v7.1A
 
 set XP_OPTS=
 set SSL_OPTS=-openssl
-set GL_OPTS=-opengl es2 -angle
+set GL_OPTS=-opengl desktop
+:: -opengl es2 -angle
 :: for clang
 ::set CPATH=%THIS_DIR%include\c++\4.8.0;%THIS_DIR%x86_64-w64-mingw32\include;%THIS_DIR%include\c++\4.8.0\x86_64-w64-mingw32
 ::set LIBRARY_PATH=%THIS_DIR%x86_64-w64-mingw32\lib
@@ -70,7 +79,7 @@ goto END
 @echo ...
 :: may be called from vc prompt, so %PATH% should be kept
 :: GNUWin32's link.exe conflicts with vc, we should not add it to PATH
-set PATH=%THIS_DIR%bin;%PERL_BIN%;%SystemRoot%\system32;%SystemRoot%;%QTSRCDIR%;%PATH%
+set PATH=%THIS_DIR%bin;%PERL_BIN%;%SystemRoot%\system32;%SystemRoot%;%PATH%
 
 cl 2>%TEMP%\testarch.txt
 findstr /i x64 %TEMP%\testarch.txt  1>nul && set ARCH=x64
@@ -109,17 +118,17 @@ goto setqt
 @echo NOTE: ensure no sh.exe in your %%PATH%%
 @echo ...
 @set MAKE_COMMAND=mingw32-make -j4
-set PATH=%THIS_DIR%bin;%GNUWIN32_BIN%;%MINGW_BIN%;%PERL_BIN%;%SystemRoot%\system32;%SystemRoot%;%QTSRCDIR%;
+set PATH=%THIS_DIR%bin;%GNUWIN32_BIN%;%MINGW_BIN%;%PERL_BIN%;%SystemRoot%\system32;%SystemRoot%;
 for /f "delims=" %%t in ('gcc -dumpmachine') do set ARCH=%%t
 goto setqt
 
 :setqt
-set BUILDQT_OUT=qt-%QMAKESPECNAME%-%ARCH%%SUFFIX%
+set BUILDQT_OUT=qt-%BUILD_DIR_SUFFIX%-%ARCH%%SUFFIX%
 md %BUILDQT_OUT%
 cd %BUILDQT_OUT%
-set PATH=%THIS_DIR%%BUILDQT_OUT%\bin;%PATH%
+set PATH=%THIS_DIR%%BUILDQT_OUT%\bin;%QTSRCDIR%\bin;%QTSRCDIR%;%PATH%
 
-set QT5OPT=-release -opensource -confirm-license -platform %QMAKESPECNAME%  -developer-build -ltcg -c++11 -no-freetype  -qt-sql-sqlite  -no-iconv -qt-style-windowsvista -nomake tests  -nomake examples  -mp  %XP_OPTS% %GL_OPTS% %SSL_OPTS%
+set QT5OPT=-release -opensource -confirm-license -platform %QMAKESPECNAME%  -developer-build -ltcg -c++11 -no-freetype  -qt-sql-sqlite -no-icu -no-iconv -qt-style-windowsvista -nomake tests  -nomake examples  -mp  %XP_OPTS% %GL_OPTS% %SSL_OPTS%
 set QT5OPT_NOCXX11=-release -opensource -confirm-license -platform %QMAKESPECNAME% -developer-build -ltcg -no-c++11 -no-freetype -qt-sql-sqlite  -qt-style-windowsxp -qt-style-windowsvista -nomake tests -nomake examples %XP_OPTS%  %GL_OPTS% %SSL_OPTS%
 set QT4OPT=-developer-build  -opensource -confirm-license -platform %QMAKESPECNAME% -ltcg -release -shared -fast -stl -qt-sql-sqlite -no-qt3support -no-xmlpatterns -no-declarative-debug -nomake demos -nomake examples -nomake docs  -nomake tests -qt-libpng -qt-libtiff -qt-libjpeg -qt-libmng -script -scripttools -no-webkit  -qt-style-windowsxp -qt-style-windowsvista  -opengl desktop -openssl %XP_OPTS%
 
@@ -127,7 +136,7 @@ set QT4OPT=-developer-build  -opensource -confirm-license -platform %QMAKESPECNA
 
 set INCLUDE=%INCLUDE%;%OPENSSL_DIR%\include;%DXSDK_DIR%Include;%WindowsSdkDir%Include\um\;%WindowsSdkDir%\Include\shared\
 set LIB=%LIB%;%OPENSSL_DIR%\lib;%DXSDK_DIR%Lib\%ARCH%;%WindowsSdkDir%\Lib\win8\um\%ARCH%\;%WindowsSdkDir%\Lib\winv6.3\um\%ARCH%\
-set PATH=%PATH%;%DXSDK_DIR%Utilities\bin\%ARCH%
+set PATH=%PATH%;%PYTHON_BIN%;%DXSDK_DIR%Utilities\bin\%ARCH%
 @echo PATH=%PATH%
 @echo INCLUDE=%INCLUDE%
 @echo LIB=%LIB%
